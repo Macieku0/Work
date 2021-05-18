be15fd1316fdbc498a532aac10b96e4ea0f6f554
Maciej Urbanski <macieku0@gmail.com>
2021-05-18 17:33:25 +0200

    Dodane komentarze

--- copy_paste.py
+++ copy_paste.py
@@ -9,12 +9,14 @@ dirPathFrom = "C:/Users/macie/Pulpit/III_CAT_SPRAWDZENIE_SRUB/20210420_FLANGE_CO
 # Scieżka do której mają być kopiowane pliki
 dirPathTo = 'C:/Users/macie/Pulpit/III_CAT_SPRAWDZENIE_SRUB/20210420_FLANGE_CONNECTIONS_I_II_III_CAT/Nowy folder'
 
+#Scieżka pod którą zostanie zapisany wykaz skopiowanych plików
 fullList = open(dirPathTo + '/' + 'wykaz_plikow.txt', 'w')
 
+#Funkcja zwracająca tylko pliki z odpowiednim rozszerzeniem
 def list (dir, ext):
     return (file for file in os.listdir(dir) if file.endswith('.' + ext))
 
-
+#Funckja główna
 for root,dirs,files in os.walk(dirPathFrom):
     pdfs = list(os.path.join(root),extension)
     for pdf in pdfs:
@@ -23,7 +25,9 @@ for root,dirs,files in os.walk(dirPathFrom):
         entry =  str(i) + '. ' + pdf + 'z folderu ' + str(os.path.join(root)) + '\n' 
         lista.append(entry)
 
+#Wiadomość końcowa
 print('Skończone, skopiowano', i,'plików z rozszerzeniem .'+ extension)
 
+#Zapis wykazu
 fullList.writelines(lista)
 fullList.close()
