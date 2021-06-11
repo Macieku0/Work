63bc77431d6a79bda62534831753c0e536cc6141
Macieku0 <73639598+Macieku0@users.noreply.github.com>
2021-06-11 10:15:25 +0200

    Cleaned

--- Count_pdf_pages.py
+++ Count_pdf_pages.py
@@ -1,4 +1,3 @@
-from numpy import roots
 import pandas as pd
 import os
 from PyPDF2 import PdfFileReader
@@ -12,7 +11,7 @@ dirPathTo = 'C:/Users/macie/Pulpit/III_CAT_SPRAWDZENIE_SRUB/PDF/'
 df = pd.DataFrame(columns=['fileName', 'fileLocation', 'pageNumber'])
 
 #Directory to save report
-Report = open(dirPathTo + '/' + 'Count_pages_report.csv', 'w')
+Report = open(f'{dirPathTo}/Count_pages_report.csv', 'w')
 #Main loop
 for root, dirs, files in os.walk(dirPathTo):
     for f in files:
@@ -25,12 +24,13 @@ for root, dirs, files in os.walk(dirPathTo):
             df2 = pd.DataFrame([[f, os.path.join(root, f), pdf.getNumPages()]], columns=['fileName', 'fileLocation', 'pageNumber'])
             #Append report with each file
             df = df.append(df2, ignore_index=True)
-        
 
-AllPages = "Sum of all pages |" + str(pdf_pages) +"\n\nBelow list of all documents\n \n"
+
+AllPages = f"Sum of all pages |{str(pdf_pages)}\n\nBelow list of all documents\n \n"
 #Save report
 Report.writelines(AllPages)
 Report.writelines(df.to_csv(columns=['fileName', 'fileLocation', 'pageNumber'], sep='|',index_label="Index", line_terminator="\n"))
 Report.close()
 #Ending code message
-print('Policzono wszystkie strony plików pdf pod ścieżką: ' + dirPathTo + '.\nGdzie również zapisno raport końcowy o nazwie: Count_pages_report.txt')
+print(f'Policzono wszystkie strony plików pdf pod ścieżką: {dirPathTo}.\nGdzie również zapisno raport końcowy o nazwie: Count_pages_report.txt')
+
